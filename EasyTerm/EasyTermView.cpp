01618958740dc68fe36b7f50f10b4a5a3564d108
dudska <songyn73@naver.com>
2020-02-22 20:54:59 +0900

    초기 커밋.

---
+++ EasyTerm/EasyTermView.cpp
@@ -0,0 +1,135 @@
+﻿
+// EasyTermView.cpp: CEasyTermView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "EasyTerm.h"
+#endif
+
+#include "EasyTermDoc.h"
+#include "EasyTermView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CEasyTermView
+
+IMPLEMENT_DYNCREATE(CEasyTermView, CFormView)
+
+BEGIN_MESSAGE_MAP(CEasyTermView, CFormView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CFormView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CFormView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CEasyTermView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CEasyTermView 생성/소멸
+
+CEasyTermView::CEasyTermView() noexcept
+	: CFormView(IDD_EASYTERM_FORM)
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CEasyTermView::~CEasyTermView()
+{
+}
+
+void CEasyTermView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BOOL CEasyTermView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CEasyTermView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+
+}
+
+
+// CEasyTermView 인쇄
+
+
+void CEasyTermView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CEasyTermView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CEasyTermView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CEasyTermView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CEasyTermView::OnPrint(CDC* pDC, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 여기에 사용자 지정 인쇄 코드를 추가합니다.
+}
+
+void CEasyTermView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CEasyTermView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CEasyTermView 진단
+
+#ifdef _DEBUG
+void CEasyTermView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CEasyTermView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CEasyTermDoc* CEasyTermView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CEasyTermDoc)));
+	return (CEasyTermDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CEasyTermView 메시지 처리기
---
+++ EasyTerm/EasyTermView.h
@@ -0,0 +1,59 @@
+﻿
+// EasyTermView.h: CEasyTermView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CEasyTermView : public CFormView
+{
+protected: // serialization에서만 만들어집니다.
+	CEasyTermView() noexcept;
+	DECLARE_DYNCREATE(CEasyTermView)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_EASYTERM_FORM };
+#endif
+
+// 특성입니다.
+public:
+	CEasyTermDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CEasyTermView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // EasyTermView.cpp의 디버그 버전
+inline CEasyTermDoc* CEasyTermView::GetDocument() const
+   { return reinterpret_cast<CEasyTermDoc*>(m_pDocument); }
+#endif
+
